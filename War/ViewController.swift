2d1ecee442e51314ad51eb0e3fa1101fd57ba6a7
hughferguson <hughferguson25@gmail.com>
2017-05-29 11:36:39 -0400

    Initial Commit

---
+++ War/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Hugh Ferguson on 5/29/17.
+//  Copyright © 2017 Hugh Ferguson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
